8a990f2aba7ebbd085fd42fd08109e1331eba8b4
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-27 15:59:43 -0300

    Exercício 5

---
+++ src/EstruturaSequencial/Ex5_Converter_metros_em_cm.java
@@ -0,0 +1,21 @@
+package EstruturaSequencial;
+
+/**
+ * Exercício 5
+ * Faça um Programa que converta metros para centímetros.
+ */
+
+import java.util.Scanner;
+
+public class Ex5_Converter_metros_em_cm {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor em metros: ");
+        double metros = input.nextDouble();
+
+        System.out.println("Resultado: " + metros*100 + "cm");
+    }
+}
