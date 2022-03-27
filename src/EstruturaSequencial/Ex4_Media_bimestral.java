de279d30c9675fa15fd129eb4d1f52c100794115
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-27 15:51:21 -0300

    Exercicio 4

---
+++ src/EstruturaSequencial/Ex4_Media_bimestral.java
@@ -0,0 +1,27 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Ex4_Media_bimestral {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double[] notas = new double[4];
+        double sum = 0;
+        double media = 0;
+
+        for(int i=0; i<4; i++) {
+            int posicao = i;
+            System.out.print("Digite a " + ++posicao + "ª nota: ");
+            notas[i] = input.nextDouble();
+        }
+        for(double value : notas) {
+            sum += value;
+        }
+
+        media = sum/notas.length;
+
+        System.out.println(media);
+    }
+}
