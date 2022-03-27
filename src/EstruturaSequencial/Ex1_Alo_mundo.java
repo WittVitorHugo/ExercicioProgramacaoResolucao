4881546450195eaaeda32718bd4016b0bac73b40
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-27 15:18:53 -0300

    Exercicio 1

---
+++ src/EstruturaSequencial/Ex1_Alo_mundo.java
@@ -0,0 +1,14 @@
+package EstruturaSequencial;
+
+/**
+ * Exercício 01
+ * Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+ */
+
+public class Ex1_Alo_mundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Alo mundo");
+    }
+}
