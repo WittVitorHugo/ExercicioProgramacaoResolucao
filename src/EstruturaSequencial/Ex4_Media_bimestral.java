e91629995df6c5f60c8611c9fddfcfa216d09db6
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-03-27 15:51:43 -0300

    Exercicio 4

--- src/EstruturaSequencial/Ex4_Media_bimestral.java
+++ src/EstruturaSequencial/Ex4_Media_bimestral.java
@@ -9,7 +9,7 @@ public class Ex4_Media_bimestral {
         Scanner input = new Scanner(System.in);
         double[] notas = new double[4];
         double sum = 0;
-        double media = 0;
+        double media;
 
         for(int i=0; i<4; i++) {
             int posicao = i;
