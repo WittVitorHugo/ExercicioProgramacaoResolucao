fbfb7eff3b08031ece556175cd5da2f5c70d4cdb
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-04-05 20:35:33 -0300

    Exercício 12

---
+++ src/EstruturaSequencial/Ex12_Peso_ideal.java
@@ -0,0 +1,21 @@
+package EstruturaSequencial;
+
+// Tendo como dados de entrada a altura de uma pessoa,
+// construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+
+import java.util.Scanner;
+
+public class Ex12_Peso_ideal {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite a altura em cm: ");
+        double altura = input.nextDouble();
+
+        double pesoIdeal = (72.7*(altura/100)) - 58;
+
+        System.out.println("Peso ideal: " + String.format("%.2f", pesoIdeal) + "Kg");
+    }
+}
